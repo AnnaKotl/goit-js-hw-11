db2251130dc006e0d1d06b94fe6101de9cc72de7
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-03 18:37:12 +0300

    library install

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+// axios.get('/users')
+//   .then(res => {
+//     console.log(res.data);
+//   });
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// https://pixabay.com/api/
+// API:38045960-ef970a60611d776386f7db4c1
+

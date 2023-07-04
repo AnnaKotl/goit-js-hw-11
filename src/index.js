3457f2849fcd1c04c7083efc6417f3c4bad02f83
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-04 20:04:31 +0300

    css + kay

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import axios from "axios";
-// axios.get('/users')
-//   .then(res => {
-//     console.log(res.data);
-//   });
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-// https://pixabay.com/api/
-// API:38045960-ef970a60611d776386f7db4c1
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '38045960-ef970a60611d776386f7db4c1';
+
+const formEl = document.querySelector('.search-form');
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+//   const searchQuery = formEl.searchQuery.value.trim();
+
+//   if (searchQuery === '') {
+//     return;
+//   }
+
+//   searchImages(searchQuery);
+});
+
 

cfb69e42ff47fbe076cda8d73d2270c82c72d2cc
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-05 23:20:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BASE_URL = 'http://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '38045960-ef970a60611d776386f7db4c1';
 
 const formEl = document.querySelector('.search-form');
